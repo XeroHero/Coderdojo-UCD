95b65f7c86f4b979727f11aa9389fc3d57544eee
Lorenzo <Lorenzo.battilocchi@ucdconnect.ie>
2020-02-29 18:37:27 +0000

    Reorganize repo

---
+++ Python/inputConditional.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+if (name == "Lorenzo" or name == "lorenzo"):
+	print("Hi Lorenzo!")
+
+elif (name == "Skye" or name == "skye"):
+	print("Hi Skye!")
+
+else:
+	print("Hi " + str(name) + ". Sorry, but I don't recognize you.")
+
+
